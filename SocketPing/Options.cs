44f0e081bcd8b12fc1dfa1fafe9fb7416fee00d4
kentxxq <admin@kentxxq.com>
2021-06-17 17:05:59 +0800

    添加项目文件。

---
+++ SocketPing/Options.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using CommandLine;
+using CommandLine.Text;
+
+namespace SocketPing
+{
+    public class Options
+    {
+        [Usage(ApplicationAlias = "sp")]
+        public static IEnumerable<Example> Examples
+        {
+            get
+            {
+                return new List<Example>() {
+                        new Example("connect host kentxxq.com,port 443", new Options { Url = "kentxxq.com:443" })
+                };
+            }
+        }
+
+        [Value(0, MetaName = "url", HelpText = "ip:port", Required = true)]
+        public string Url { get; set; } = null!;
+
+        [Option('n', "retryTimes", Required = false, HelpText = "default:0,retry forever")]
+        public int RetryTimes { get; set; } = 0;
+
+        [Option('t', "timeout", Required = false, HelpText = "default:2 seconds")]
+        public int Timeout { get; set; } = 2;
+
+        [Option('q', "quit", Required = false, HelpText = "Quit after connection succeeded")]
+        public bool Quit { get; set; } = false;
+    }
+}
---
+++ SocketPing/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Sockets;
+using CommandLine;
+
+namespace SocketPing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Parser.Default.ParseArguments<Options>(args)
+                          .WithParsed<Options>(o =>
+                          {
+                              IPEndPoint endpoint = null!;
+                              bool result;
+                              try
+                              {
+                                  var host = o.Url.Split(":")[0];
+                                  var ip = Dns.GetHostAddresses(host)[0];
+                                  var port = o.Url.Split(":")[1];
+                                  endpoint = new IPEndPoint(ip, int.Parse(port));
+                              }
+                              catch (Exception)
+                              {
+                                  Console.WriteLine("地址解析失败");
+                                  Environment.Exit(1);
+                              }
+
+
+                              if (o.RetryTimes == 0)
+                              {
+                                  while (true)
+                                  {
+                                      result = TestConnect(endpoint, o.Timeout);
+                                      if (result && o.Quit)
+                                      {
+                                          Environment.Exit(0);
+                                      }
+                                  }
+                              }
+                              else
+                              {
+                                  for (int i = 0; i < o.RetryTimes; i++)
+                                  {
+                                      result = TestConnect(endpoint, o.Timeout);
+                                      if (result && o.Quit)
+                                      {
+                                          Environment.Exit(0);
+                                      }
+                                  }
+                                  Environment.Exit(1);
+                              }
+
+                          });
+        }
+
+        static bool TestConnect(IPEndPoint iPEndPoint, int timeout)
+        {
+            using (var tcp = new TcpClient())
+            {
+                var stopwatch = new Stopwatch();
+                stopwatch.Start();
+                var result = tcp.ConnectAsync(iPEndPoint.Address, iPEndPoint.Port).Wait(timeout * 1000);
+                stopwatch.Stop();
+                if (result)
+                {
+                    Console.WriteLine($"{iPEndPoint}:连接成功,耗时{stopwatch.ElapsedMilliseconds}毫秒");
+                    return true;
+                }
+                else
+                {
+                    Console.WriteLine($"{iPEndPoint}:连接失败,耗时{stopwatch.ElapsedMilliseconds}毫秒");
+                    return false;
+                }
+            }
+        }
+    }
+}
